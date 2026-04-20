e8c38205ba17b01a25e6391c3465696452e3b250
Cobble <cobblestonerules04@gmail.com>
2026-04-20 12:57:05 -0500

    Limiting one account per email.

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
